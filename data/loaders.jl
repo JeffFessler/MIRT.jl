4824a806247a184a585caf2171666efe05413582
fessler <fessler@umich.edu>
2019-02-23 14:38:26 -0500

    fix

--- data/loaders.jl
+++ data/loaders.jl
@@ -2,8 +2,9 @@
 # utilities for loading in data / images
 
 using FileIO
+using Colors
 
 function ir_load_brainweb_t1_256()
 	file = dirname(pathof(MIRT)) * "../data/mri/brainweb_t1_256.gif"
-	return Float32.(Gray.(load(file)))
+	return Float32.(Gray.(load(file)))' # transpose!
 end
