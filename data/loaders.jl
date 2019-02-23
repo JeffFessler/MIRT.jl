e3e3df1a4f6c0a811934bae4cf8bfbd1bae91ef9
fessler <fessler@umich.edu>
2019-02-23 14:44:57 -0500

    fix path

--- data/loaders.jl
+++ data/loaders.jl
@@ -5,6 +5,6 @@ using FileIO
 using Colors
 
 function ir_load_brainweb_t1_256()
-	file = dirname(pathof(MIRT)) * "../data/mri/brainweb_t1_256.gif"
+	file = dirname(pathof(MIRT)) * "/../data/mri/brainweb_t1_256.gif"
 	return Float32.(Gray.(load(file)))' # transpose!
 end
