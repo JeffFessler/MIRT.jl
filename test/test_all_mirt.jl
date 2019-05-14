5c60b2493c090b110d550879cfff6bfab15df77f
Jeff Fessler <fessler@jf13.local>
2019-05-14 16:18:54 -0400

    fix path

--- test/test_all_mirt.jl
+++ test/test_all_mirt.jl
@@ -21,11 +21,14 @@ run all MIRT tests
 """
 function test_all_mirt()
 
+	basedir = dirname(pathof(MIRT))
 	@testset "all MIRT" begin
 		for root in srclist
 		#	@show root
 			@testset "$root" begin
-				include("../src/" * root * "/z-test.jl")
+				tmp = joinpath(basedir, root, "z-test.jl")
+			#	printstyled(tmp * "\n", color=:blue, bold=true)
+				include(tmp)
 			end
 		end
 	end
