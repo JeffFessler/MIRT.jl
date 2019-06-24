7bef81cbeaf1a9060f9fc80e918648b7e9708149
Jeff Fessler <fessler@ir71.eecs.umich.edu>
2019-06-24 12:56:27 -0400

    Add plot setup for linux

--- src/plot/z-list.jl
+++ src/plot/z-list.jl
@@ -1,3 +1,7 @@
-include("jim.jl")
+# plot/z-list.jl
 
+include("jim.jl")
 export jim
+
+include("setup.jl")
+export mirt_setup_plot
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,6 @@
 
 using MIRT
 
+mirt_setup_plot() # see src/plot/setup.jl
+
 test_all_mirt()
