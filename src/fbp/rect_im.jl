a9fff17016b3b0575a298aad0f069059056b4407
Helena <helenasheinonen@gmail.com>
2019-06-26 14:23:32 -0400

    added my own rects and fixed bugs

--- src/fbp/rect_im.jl
+++ src/fbp/rect_im.jl
@@ -8,8 +8,8 @@ generate rectangle phantom image from parameters:
     `[x_center y_center x_width y_width angle_degrees amplitude]`
 
 in
-    `ig`			image_geom() object
-    `params`		[Nrect,6] rect parameters
+    `ig`						image_geom() object
+    `params`		[Nrect,6]	rect parameters. if empty use default
 
 options
     `oversample`	int     oversampling factor, for grayscale boundaries
@@ -26,7 +26,7 @@ out
 
 """
 function rect_im(ig::MIRT_image_geom,
-    params::abstractArray{<:Real,2};
+    params::AbstractArray{<:Real,2};
     oversample::Integer=1,
     hu_scale::Real=1,
     fov::Real=ig.fov,
@@ -35,20 +35,23 @@ function rect_im(ig::MIRT_image_geom,
 	replace::Bool=false,
 	return_params::Bool=false)
 
-    args = (ig.nx, ig.ny, ig.dx, ig.dy, ig.offset_x, ig.offset_y, replace)
-
-    if oversample > 1
-    #    ig = ig.over(oversample)
-		@warn("not yet done")
+	if oversample > 1
+    	ig = ig.over(oversample)
     end
+    args = (ig.nx, ig.ny, ig.dx, ig.dy, ig.offset_x, ig.offset_y, replace)
 
 	if isempty(fov)
 		fov = ig.fov
 	end
+	#=
+	if isempty(params)
+		params = rect_im_default_parameters(xfov, yfov)
+	end
+	=#
 
     params[:,6] .*= hu_scale
 
-    do_fast = params[:,5] == 0 # default for :auto
+    do_fast = params[:,5] .== 0 # default for :auto
     if how == :fast
             do_fast[:] .= true
     elseif how == :slow
@@ -63,8 +66,12 @@ function rect_im(ig::MIRT_image_geom,
         phantom += rect_im_fast(params[do_fast,:], args...)
 	end
 
-	if any(!do_fast)
-		phantom += rect_im_slow(params[!do_fast,:], args..., over)
+	if any(.!do_fast)
+		phantom += rect_im_slow(params[.!do_fast,:], args..., oversample)
+	end
+
+	if oversample > 1
+		phantom = downsample2(phantom, oversample)
 	end
 
 	if return_params
@@ -84,11 +91,10 @@ function rect_im_fast(params_in, nx, ny, dx, dy, offset_x, offset_y, replace)
 
 	phantom = zeros(Float32, nx, ny)
 
-	wx = (nx-1)/2 + offset_x
+	wx = (nx-1)/2 + offset_x # scalars
 	wy = (ny-1)/2 + offset_y
-	x1 = ([0:nx-1]' - wx) * dx
-	y1 = ([0:ny-1] - wy) * dy
-
+	x1 = ((0:nx-1) .- wx) * dx # arrays
+	y1 = ((0:ny-1) .- wy) * dy
 	fun = (x1, x2, wx) -> # integrated rect(x/wx) function from x1 to x2
 		max(min(x2, wx/2) - max(x1, -wx/2), 0)
 
@@ -105,52 +111,55 @@ function rect_im_fast(params_in, nx, ny, dx, dy, offset_x, offset_y, replace)
 		if theta != 0
 			throw("theta=0 required")
 		end
-		x = x1 - cx
-		y = y1 - cy
-		tx = fun(x-abs(dx)/2, x+abs(dx)/2, wx) / abs(dx)
-		ty = fun(y-abs(dy)/2, y+abs(dy)/2, wy) / abs(dy)
-		tmp = single(tx) * single(ty) # outer product (separable)
+		x = x1 .- cx
+		y = y1 .- cy
+		tx = fun.(x.-abs(dx)/2, x.+abs(dx)/2, wx) / abs(dx)
+		ty = fun.(y.-abs(dy)/2, y.+abs(dy)/2, wy) / abs(dy)
+		tmp = Float32.(tx) * Float32.(ty)' # outer product (separable)
 		if replace
-			phantom(tmp > 0) = rect[6]
+			phantom[tmp .> 0] .= rect[6]
 		else
 			phantom = phantom + rect[6] * tmp
 		end
+	end
 	return phantom
 end
 
 """
 `phantom = rect_im_slow()`
 """
-function rect_im_slow(params_in, ig.nx, ig.ny, ig.dx, ig.dy, ig.offset_x, ig.offset_y, replace, over)
-
+function rect_im_slow(params_in, nx, ny, dx, dy, offset_x, offset_y, replace, over)
+	params = copy(params_in)
 	if size(params,2) != 6
 		throw("bad rect parameter vector size")
 	end
-
 	phantom = zeros(Float32, nx*over, ny*over)
 
-	wx = (nx*over - 1)/2 + offset_x*over
+	wx = (nx*over - 1)/2 + offset_x*over # scalar
 	wy = (ny*over - 1)/2 + offset_y*over
-	xx = ([0:nx*over-1] - wx) * dx / over
-	yy = ([0:ny*over-1] - wy) * dy / over
+	xx = ((0:nx*over-1) .- wx) * dx / over # Array{Float64,2}
+	yy = ((0:ny*over-1) .- wy) * dy / over
 	(xx, yy) = ndgrid(xx, yy)
-
 	# ticker reset
 	ne = size(params)[1]
 	for ie in 1:ne
-		ticker(mfilename, ie, ne)
+		#ticker(mfilename, ie, ne)
 
 		rect = params[ie, :]
-		cx = rect[1]
+		cx = rect[1] # float64
 		wx = rect[3]
 		cy = rect[2]
 		wy = rect[4]
-		theta = rect[5] * (pi/180)
-		x = cos(theta) * (xx-cx) + sin(theta) * (yy-cy)
-		y = -sin(theta) * (xx-cx) + cos(theta) * (yy-cy)
-		tmp = abs(x / wx) < 1/2 & abs(y / wy) < 1/2 # ?
+		theta = rect[5] * (pi/180) #float64
+
+		x = cos(theta) .* (xx.-cx) + sin(theta) .* (yy.-cy) #Array{Float64,2}
+		y = -sin(theta) .* (xx.-cx) + cos(theta) .* (yy.-cy) #Array{Float64,2}
+		#typeof(x / wx) is Array{Float64,2}
+		#typeof(abs.(x / wx) .< 1/2) is a bit array
+		tmp = (abs.(x / wx) .< 1/2) .& (abs.(y / wy) .< 1/2) # typeof(tmp) ?
+
 		if replace
-			phantom[tmp > 0] = rect[6] # ?
+			phantom[tmp .> 0] .= rect[6]
 		else
 			phantom = phantom + rect[6] * tmp
 		end
@@ -158,6 +167,101 @@ function rect_im_slow(params_in, ig.nx, ig.ny, ig.dx, ig.dy, ig.offset_x, ig.off
 	return phantom
 end
 
+"""
+`phantom = rect_im(nx, dx, params; args...)`
+
+square image of size `nx` by `nx`,
+specifying pixel size `dx` and rect `params`
+"""
+function rect_im(nx::Integer, dx::Real, params; args...)
+	ig = image_geom(nx=nx, dx=1)
+	return rect_im(ig, params; args...)
+end
+
+"""
+`phantom = rect_im(nx::Integer, params; args...)`
+
+square image of size `nx` by `nx` with
+pixel size `dx=1` and ellipse `params`
+"""
+function rect_im(nx::Integer, params; args...)
+	return ellipse_im(nx, 1., params; args...)
+end
+
+"""
+`phantom = rect_im(nx::Integer; ny::Integer=nx, dx::Real=1)`
+
+image of size `nx` by `ny` (default `nx`) with specified `dx` (default 1),
+defaults to `:my_rect`
+"""
+function rect_im(nx::Integer; ny::Integer=nx, dx::Real=1, args...)
+	if image_geom(nx=nx, ny=ny, dx=dx, args...)
+		return rect_im(ig, :my_rect; args...)
+	end
+end
+
+"""
+`phantom = rect_im(nx::Integer, ny::Integer; args...)`
+
+`:my_rect` of size `nx` by `ny`
+"""
+function rect_im(nx::Integer, ny::Integer; args...)
+	return rect_im(nx, ny=ny, dx=1.; args...)
+end
+
+"""
+`phantom = rect_im(ig, code, args...)`
+
+`code = :my_rect | :default`
+"""
+function rect_im(ig::MIRT_image_geom, params::Symbol; oversample, chat, args...)
+	fov = ig.fov
+	if params == :my_rect
+		params = my_rect(fov, fov)
+	elseif params == :default
+		params = rect_im_default_parameters(fov, fov)
+	elseif params == :smiley
+		params = smiley_parameters(fov, fov)
+	else
+		throw("bad phantom symbol $params")
+	end
+	return rect_im(ig, params; args...)
+end
+
+"""
+`phantom = rect_im(ig; args...)`
+
+`:default` (default) for given image geometry `ig`
+"""
+function rect_im(ig::MIRT_image_geom; args...)
+	return rect_im(ig, :default; args...)
+end
+
+
+
+"""
+`(xx,yy) = ndgrid(x,y)`
+"""
+function ndgrid(x::AbstractVector{<:Number},
+				y::AbstractVector{<:Number})
+	return (repeat(x, 1, length(y)), repeat(y', length(x), 1))
+end
+
+"""
+`(xr,yr) = rot2(x, y, theta)`
+2D rotation
+"""
+function rot2(x, y, theta)
+	xr = cos(theta) * x + sin(theta) * y
+	yr = -sin(theta) * x + cos(theta) * y
+	return (xr, yr)
+end
+
+"""
+`params = rect_im_default_parameters(xfov, yfov)`
+
+default parameters
+"""
 function rect_im_default_parameters(xfov, yfov)
 	f = 1/64
 	params = [
@@ -173,26 +277,46 @@ function rect_im_default_parameters(xfov, yfov)
 		18    0     1      1      0     1
 	]
 
-	params[:,[1,3]] .*= xfov/64
-	params[:,[2,4]] .*= yfov/64
+	params[:,[1,3]] .*= xfov/64 # x_center and x_width
+	params[:,[2,4]] .*= yfov/64 # y_center and y_width
 
 	return params
 end
 
 
-
-#=
-if isempty(params)
-	params = rect_im_default_parameters(xfov, yfov)
+"""
+`params = my_rect(xfov, yfov)`
+"""
+function my_rect(xfov, yfov)
+	f=1/64
+	rect = [
+	35	35	20	10	45	1
+	35	-35	20	10	-45	1
+	-35	-35	20	10	45	1
+	-35	35	20	10	-45	1
+	0	0	40	40	45	1
+	0	0	40	40	0	0.5
+	40	0	12	1.5	0	1.5
+	-40	0	12	1.5	0	1.5
+	]
+	return rect
 end
-=#
 
 """
-`(xx,yy) = ndgrid(x,y)`
+`params = smiley_parameters(xfov, yfov)`
+
+smiley face out of rects
 """
-function ndgrid(x::AbstractVector{<:Number},
-				y::AbstractVector{<:Number})
-	return (repeat(x, 1, length(y)), repeat(y', length(x), 1))
+function smiley_parameters(xfov, yfov)
+	rect = [
+	0	0	80	80	0	0.5
+	-20	-20	10	15	0	1 #eyes
+	20	-20	10	15	0	1
+	0	25	50	6	0	1 #mouth
+	-23	19	4	6	0	1
+	23	19	4	6	0	1
+	]
+	return rect
 end
 
 """
@@ -204,19 +328,33 @@ function rect_im()
 	@doc rect_im
 end
 
-
 """
 `rect_im_show()`
 """
-function rect_im_show() # ?
+function rect_im_show()
 	#plot(1:10, 1:10)
-	ig = image_geom(nx=2^8, ny=2^8, fov=100) # ?
-	x0 = rect_im(ig, [[0.5, 0, 3, 20].*ig.dx, 0, 1], oversample=3)
-	p1 = jim(x0)
-	x0 = rect_im(ig, [], oversample=3, chat=true, im)
-	p2 = jim(ig.x, ig.y, x0, title="default rects")
+	ig = image_geom(nx=2^8, ny=2^8, fov=100)
 
-	plot(p1, p2)
+	x0 = rect_im(ig, [[[0.5, 0, 3, 20]*ig.dx..., 0, 1]';], oversample=3)
+	p1 = jim(x0)
+
+	x1 = rect_im(ig, :default; oversample=3, chat=true)
+	p2 = jim(x1, title="default rects")
+
+	x2 = rect_im(ig, :my_rect; oversample=3, chat=true)
+	p3 = jim(x2, title="my rect")
+
+	x3 =rect_im(ig, :smiley; oversample=3, chat=true)
+	p4 = jim(x3, title="smiley")
+
+	plot(p1, p2, p3, p4)
+end
+
+function rect_im_test()
+	fov = 100
+	rect_im_default_parameters(fov, fov)
+	ellipse_im_show()
+	true
 end
 
 
