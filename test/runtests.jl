3a8cf2d6bbe49bb1dcd518eb1c25544e16bb3840
fessler@umich.edu <fessler@umich.edu>
2019-07-09 13:11:58 -0400

    Add ENV for GKS

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,17 +4,11 @@ using MIRT
 
 using Plots: default
 
-if !isinteractive()
+if !isinteractive() # for usual noninteractive test, do not show plots
 	default(show=false, reuse=true) # trick from Plots/runtests.jl
+	ENV["GKSwstype"] = "100" # also from Plots/runtests.jl
 end
-
-# the following line is a trick that avoid plots during tests
-# that take time and make CI output messages about "GKS"
-# actually this line is no longer needed because of default() above!
-# jim(:show, false)
 
 mirt_setup_plot() # see src/plot/setup.jl
 
 test_all_mirt()
-
-# jim(:show, true) # return to default state where images are shown
