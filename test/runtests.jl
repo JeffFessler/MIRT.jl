611fe74d5d0151bdb60d7ad23f1d93712485da40
fessler@umich.edu <fessler@umich.edu>
2019-07-09 12:28:41 -0400

    Make plot default false for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,19 @@
 
 using MIRT
 
+using Plots: default
+
+if !isinteractive()
+	default(show=false, reuse=true) # trick from Plots/runtests.jl
+end
+
 # the following line is a trick that avoid plots during tests
 # that take time and make CI output messages about "GKS"
-jim(:show, false)
+# actually this line is no longer needed because of default() above!
+# jim(:show, false)
 
 mirt_setup_plot() # see src/plot/setup.jl
 
 test_all_mirt()
 
-jim(:show, true) # return to default state where images are shown
+# jim(:show, true) # return to default state where images are shown
