940a98edc5a61fb2403a5485caa6c0b42c5ad419
fessler@umich.edu <fessler@umich.edu>
2019-07-09 12:03:40 -0400

    Add :show

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@
 
 using MIRT
 
+# the following line is a trick that avoid plots during tests
+# that take time and make CI output messages about "GKS"
+jim(:show, false)
+
 mirt_setup_plot() # see src/plot/setup.jl
 
 test_all_mirt()
+
+jim(:show, true) # return to default state where images are shown
