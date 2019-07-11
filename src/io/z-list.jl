597f0785df94be1c202f912071d4b2ca6fbf0b8f
Jeff Fessler <fessler@umich.edu>
2019-07-10 21:05:19 -0400

    Add @shows

--- src/io/z-list.jl
+++ src/io/z-list.jl
@@ -1,25 +1,11 @@
 # io/z-list.jl
 
 include("caller_name.jl")
-export caller_name
-
 include("fld.jl")
-export fld_header
-export fld_read
-
 include("fld-write.jl")
-export fld_write
-export ir_test_dir!
-export ir_test_dir
-
 include("ge-mri/pfile.jl")
-export loadpfile
-
-#include("ge-mri/rdb-26_002.jl")
+#include("ge-mri/rdb-26_002.jl") # not
 include("ge-mri/read_rdb_hdr.jl")
-export read_rdb_hdr
-
 include("ir_dump.jl")
-export ir_dump
-
 include("prompt.jl")
+include("shows.jl")
--- src/io/z-test.jl
+++ src/io/z-test.jl
@@ -6,5 +6,8 @@ using Test
 @test fld_read(:test)
 @test fld_write(:test)
 @test ir_dump(:test)
-#@test loadpfile(:test)
+@test loadpfile(:test)
 @test prompt(:test)
+
+#@test shows(:test) # no - not exported
+@shows ones(3,4)
