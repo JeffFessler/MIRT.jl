7c91b0aeef54108bf1826d1584cbe58617e37e8d
fessler@umich.edu <fessler@umich.edu>
2019-07-13 01:55:16 -0400

    To tab4

--- src/fbp/ellipse_sino.jl
+++ src/fbp/ellipse_sino.jl
@@ -1,63 +1,82 @@
+#=
+ellipse_sino.jl
+2019-07-12, Helena H
+=#
+
 using Plots
 
 export ellipse_sino
 
 """
-`(sino, pos, ang) = ellipse_sino(sg, ells;
-            oversample=1, xscale=1, yscale=1)`
+`(sino, pos, ang) = ellipse_sino(sg, ells ; oversample=1, xscale=1, yscale=1)`
 
 Create sinogram projection of one or more ellipses.
 Works for both parallel-beam geometry and for fan-beam geometry.
 
 in
-* `sg`                      sinogram geometry object from sino_geom()
-* `ells`            		`[ne 6]`  parameters
-                            [centx centy radx rady angle_degrees amplitue]
-                            (or strum motion object; see ellipse_motion.m)
+- `sg::MIRT_sino_geom,`		sinogram geometry object from `sino_geom()`
+- `ells::Matrix`			`[ne 6]` ellipse parameters
+							`[centx centy radx rady angle_degrees amplitude]`
 
 options
-* `oversample::Integer`     oversampling factor for emulating "strips"
-                            default 1: just 1 ray per detector element
-* `xscale::Integer`         use -1 to flip in x (not recommended)
-* `yscale::Integer`         use -1 to flip in y (not recommended)
+- `oversample::Integer`	oversampling factor for emulating "strips"
+						default 1: just 1 ray per detector element
+- `xscale::Integer`		use -1 to flip in x (not recommended); default 1
+- `yscale::Integer`		use -1 to flip in y (not recommended); default 1
 
 
 out
-* `sino`            `[nb na]` sinogram
-* `pos`             `[nb 1]`  radial sample positions (or [nb na] if mojette)
-* `ang`             `[na]`    angular sample locations
+- `sino`		`[nb na]` sinogram
+- `pos` 		`[nb]` radial sample positions (or [nb na] if mojette)
+- `ang` 		`[na]` angular sample locations
 """
-function ellipse_sino(sg::MIRT_sino_geom,
-        ells::AbstractArray{<:Real,2};
-        oversample::Integer=1,
-        xscale::Integer=1,
-        yscale::Integer=1,
-        )
+function ellipse_sino(sg::MIRT_sino_geom, ells::AbstractMatrix{<:Real} ;
+		oversample::Integer=1,
+		xscale::Integer=1,
+		yscale::Integer=1,
+		)
 
-        if sg.how == :fan
-                (sino, pos, ang) = ellipse_sino_go(ells, sg.nb, sg.ds, sg.offset, sg.na, sg.orbit, sg.orbit_start,
-                                        sg.dso, sg.dod, sg.dfs, sg.source_offset, xscale, yscale, oversample, 0)
-        elseif sg.how == :par
-                (sino, pos, ang) = ellipse_sino_go(ells, sg.nb, sg.dr, sg.offset, sg.na, sg.orbit, sg.orbit_start,
-                                        Inf, 1, 0, sg.source_offset, xscale, yscale, oversample, 0)
-        elseif sg.how == :moj
-                (sino, pos, ang) = ellipse_sino_go(ells, sg.nb, sg.d, sg.offset, sg.na, sg.orbit, sg.orbit_start,
-					Inf, 1, 0, sg.source_offset, xscale, yscale, oversample, 0)
+#=
+		if sg.how == :fan
+			(sino, pos, ang) = ellipse_sino_go(ells, sg.nb, sg.ds, sg.offset,
+					sg.na, sg.orbit, sg.orbit_start,
+					sg.dso, sg.dod, sg.dfs, sg.source_offset,
+					xscale, yscale, oversample, 0)
+		elseif sg.how == :par
+			(sino, pos, ang) = ellipse_sino_go(ells, sg.nb, sg.dr, sg.offset,
+					sg.na, sg.orbit, sg.orbit_start,
+					Inf, 1, 0, sg.source_offset,
+					xscale, yscale, oversample, 0)
+		elseif sg.how == :moj
+			(sino, pos, ang) = ellipse_sino_go(ells, sg.nb, sg.d, sg.offset,
+					sg.na, sg.orbit, sg.orbit_start,
+					Inf, 1, 0, sg.source_offset,
+					xscale, yscale, oversample, 0)
 	else
 		throw("sino geom $(sg.how) not done") # should never happen
 	end
-        return (sino, pos, ang)
+
+	return (sino, pos, ang)
+=#
+
+	return ellipse_sino_go(ells, sg.nb, sg.d, sg.offset,
+				sg.na, sg.orbit, sg.orbit_start,
+				sg.dso, sg.dod, sg.dfs, sg.source_offset,
+				xscale, yscale, oversample, sg.how == :moj) # todo
 end
+
 
 """
 `ellipse_sino_go()`
 """
 function ellipse_sino_go(ells, nb, ds, offset_s, na, orbit, orbit_start,
-	dso, dod, dfs, source_offset, xscale, yscale, oversample, mojette)
+	dso, dod, dfs, source_offset, xscale, yscale, oversample, is_mojette)
 
-	ang = ((orbit_start .+ (0:(na - 1))' / na * orbit)) * (pi/180)
-	(pos, pos2) = ellipse_sino_pos(pos[:], nb, ds, offset_s, oversample, mojette, ang)
-	sino = ellipse_sino_do(ells, pos2, ang[:]', xscale, yscale, dso, dod, dfs, source_offset)
+	ang = (orbit_start .+ (0:(na - 1))' / na * orbit) * (pi/180)
+	(pos, pos2) =
+		ellipse_sino_pos(pos[:], nb, ds, offset_s, oversample, is_mojette, ang)
+	sino = ellipse_sino_do(ells, pos2, ang[:]',
+			xscale, yscale, dso, dod, dfs, source_offset)
 
 	if oversample != 1
 		sino = downsample2(sino, [oversample 1])
@@ -66,13 +85,15 @@ function ellipse_sino_go(ells, nb, ds, offset_s, na, orbit, orbit_start,
 	return (sino, pos, ang)
 end
 
+
 """
 `ellipse_sino_pos()`
 
 determine usual and fine "radial" sample positions
 """
-function ellipse_sino_pos(pos, nb, ds, offset_s, nover, mojette, ang)
+function ellipse_sino_pos(pos, nb, ds, offset_s, nover, is_mojette, ang)
 	wb = (nb - 1)/2 + offset_s
+
 	if mojette != 0 # tricky mojette radial sampling
 		# trick: ray_spacing aka ds comes from dx which is in mojette
 		!isempty(ds) && throw("ds must be empty for mojette case")
@@ -92,6 +113,7 @@ function ellipse_sino_pos(pos, nb, ds, offset_s, nover, mojette, ang)
 		else
 			pos_fine = pos_coarse
 		end
+
 	else # ordinary non-mojette sampling
 		pos_coarse = ((0:(nb - 1)) .- wb) .* ds' # [nb]
 		if nover > 1
@@ -114,13 +136,14 @@ end
 
 analytical line-integral projections of ellipse (fan-beam in general)
 """
-function ellipse_sino_do(ells, pos, ang, xscale, yscale, dso, dod, dfs, source_offset)
+function ellipse_sino_do(ells, pos, ang, xscale, yscale,
+		dso, dod, dfs, source_offset)
 	nb = size(pos, 1)
-	na = maximum(size(ang))
+	na = length(ang)
 	# effective radial and angular sample locations in parallel geometry
 
 	if isinf(dso) # type of dso?
-		if size(pos, 2) > 1 #mojette
+		if size(pos, 2) > 1 # mojette
 			rads = pos
 			angs = repeat(ang', nb, 1)
 		else
@@ -141,13 +164,11 @@ function ellipse_sino_do(ells, pos, ang, xscale, yscale, dso, dod, dfs, source_o
 		end
 
 		rad = dso * sin.(gam) + source_offset * cos.(gam)
-		rads = repeat(rad, 1, na) # [nb, na]
-		angs = gam .+ ang  # [nb na] gamma + beta
+		rads = repeat(rad, 1, na) # [nb na]
+		angs = gam .+ ang # [nb na] gamma + beta
 	end
 
-	# clear alf gam rad pos ang
-
-	sino = zeros(nb, na)
+	sino = zeros(Float32, nb, na)
 
 	cangs = cos.(angs)
 	sangs = sin.(angs)
@@ -155,25 +176,18 @@ function ellipse_sino_do(ells, pos, ang, xscale, yscale, dso, dod, dfs, source_o
 	#loop over ellipses
 	#ticker reset
 
-	ne = size(ells)[1]
-	size(ells)[2] != 6 && throw("6 parameters per ellipse")
+	ne = size(ells, 1)
+	size(ells,2) != 6 && throw("6 parameters per ellipse")
 	for ie in 1:ne
 		#ticker(mfilename, ie, ne)
-		if isa(ells, Type)
-			ell = ells.ell[ie, na] # [na 6]
-			ell = reshape(ell, 1, na, 6)
-			ell = repeat(ell, nb, 1, 1)
-		else
-			ell = ells[ie, :]
-			ell = reshape(ell, 1, 1, 6) # is this 3 dimensional
-		end
+		ell = ells[ie, :]
 
-		cx = xscale * ell[:, :, 1]
+		cx = xscale * ell[1]
 		rx = ell[:, :, 3]
-		cy = yscale * ell[:, :, 2]
-		ry = ell[:, :, 4]
+		cy = yscale * ell[2]
+		ry = ell[4]
 		eang = ell[5] * (pi/180)
-		val = ell[:, :, 6]
+		val = ell[6]
 
 		if yscale == -1
 			eang = -eang
@@ -184,13 +198,16 @@ function ellipse_sino_do(ells, pos, ang, xscale, yscale, dso, dod, dfs, source_o
 		scale = 2 * val .* rx .* ry
 
 		# square of projected radius:
-		rp2 = (rx .* (cangs .* cos(eang) + sangs .* sin(eang))).^2 + (ry .* (sangs .* cos(eang) - cangs .* sin(eang))).^2
+		rp2 = (rx .* (cangs .* cos(eang) + sangs .* sin(eang))).^2 +
+				(ry .* (sangs .* cos(eang) - cangs .* sin(eang))).^2
 		sp = cx .* cangs + cy .* sangs # radial shift
 		dis2 = (rads - sp).^2 # square of distances from center
 		sino += scale ./ rp2 .* sqrt.(max.(rp2 - dis2, 0))
 	end
+
 	return sino
 end
+
 
 """
 `ellipse_sino()`
@@ -201,6 +218,7 @@ function ellipse_sino()
 	@doc ellipse_sino
 end
 
+
 """
 `ellipse_sino_test()`
 
@@ -209,11 +227,8 @@ internal test routine: standard sampling
 function ellipse_sino_test()
 	down = 4
 	ig = image_geom(nx=512, ny=504, dx=1)
-	ell = [
-	40 70 50 150 20 10
-	]
+	ell = [ 40 70 50 150 20 10 ]
 	#(xtrue, ell) = ellipse_im(ig, ell; oversample=4)
-
 
 	gf = sino_geom(:fan, nb = 888, na = 984, d = 1.0, orbit = 360,
 			orbit_start = 0, offset = 0.75, dsd = 949, dod = 408, down=down)
@@ -233,15 +248,14 @@ function ellipse_sino_test()
 	e3 = ellipse_sino(gm, ell; oversample=1, xscale=-1, yscale=-1)
 end
 
+
 """
 `ellipse_sino_show()`
 """
 function ellipse_sino_show()
 	down = 4
 	ig = image_geom(nx=512, ny=504, dx=1)
-	ell = [
-	40 70 50 150 20 10
-	]
+	ell = [ 40 70 50 150 20 10 ]
 
 	#(xtrue, ell) = ellipse_im(ig, ell; oversample=4)
 
