a37c87813409f228673a99f5ac82fc6cb1f5d14e
Jeff Fessler <fessler@umich.edu>
2019-07-30 18:25:30 -0400

    Move export

--- src/utility/z-list.jl
+++ src/utility/z-list.jl
@@ -1,19 +1,6 @@
 # utility/z-list.jl
 
 include("downsample.jl")
-export downsample1
-export downsample2
-export downsample3
-export downsample
-
 include("map_many.jl")
-export map_many
-
 include("mask.jl")
-export mask_or
-export mask_outline
-export embed
-export maskit
-
 include("ndgrid.jl")
-export ndgrid
