33a0b8d354e6510ba98267da8b91a26af20bb834
Jeff Fessler <fessler@umich.edu>
2019-11-17 11:29:17 -0500

    Update extrapolation

--- src/utility/interp1.jl
+++ src/utility/interp1.jl
@@ -22,13 +22,15 @@ In:
 Option:
 - `how::Interpolations.InterpolationType` default `Gridded(Linear())`
 - `extrap::Any` how to extrapolate, e.g., `Flat()`; default `0`
+other options from Interpolations.jl are `Line()` `Periodic()` `Reflect()` `Throw()`
 
 Output is same size as input `xi`
 """
 function interp1(x::AbstractVector{<:Real}, y::AbstractVector{<:Number}, xi ;
 		how::Interpolations.InterpolationType = Gridded(Linear()),
-		extrap::Any = 0,
-	)
+		extrap::Any = 0)
+#		extrap::Union{<:Interpolations.BoundaryCondition, <:Number} = 0 # fails?
+
 	fun = interpolate((x,), y, how)
 	if !isnothing(extrap)
 		fun = extrapolate(fun, extrap)
@@ -45,14 +47,17 @@ function interp1(test::Symbol)
 	@test (@inferred interp1(1:4, 2:5, 1.5)) == 2.5
 	@test (@inferred interp1(1:2, [3im,4im], 1.5)) == 3.5im
 	x = LinRange(-1, 1, 21)
-	y = cos.(x*π)
+	y = sin.(x*π)
 	xi = LinRange(-1, 1, 101) * 2
-	y0 = @inferred interp1(x, y, xi) # 0 extrapolation
-	ye = @inferred interp1(x, y, xi, extrap=Flat())
 	@test_throws BoundsError interp1(x, y, xi, extrap=nothing)
+	@test_throws BoundsError interp1(x, y, xi, extrap=Throw())
 	scatter(x, y, label="")
-	plot!(xi, y0, label="0")
-	plot!(xi, ye, label="Flat")
+	y0 = @inferred interp1(x, y, xi) # 0 extrapolation
+	plot!(xi, y0, label="0", ylim=[-2,2])
+	for ex in (Flat(), Line(), Periodic(), Reflect())
+		yy = @inferred interp1(x, y, xi, extrap=ex)
+		plot!(xi, yy, label="$ex")
+	end
 	gui()
 	true
 end
