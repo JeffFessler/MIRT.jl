4e8e6fe499bfa4166683c91c998ccaf79bfd2a40
Jeff Fessler <JeffFessler@users.noreply.github.com>
2019-12-04 09:55:38 -0500

    Sort

--- src/utility/z-test.jl
+++ src/utility/z-test.jl
@@ -1,12 +1,12 @@
 # utility/z-test.jl
 
-using Test
+using Test: @test
 
 @test downsample(:test)
-@test map_many(:test)
-@test mask_test()
-@test ndgrid(:test)
 @test interp1(:test)
 @test jinc(:test)
+@test map_many(:test)
+@test mask_test()
 @test max_percent_diff(:test)
+@test ndgrid(:test)
 @test rect(:test)
