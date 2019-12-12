597ad5d8c9a40218c6cbe1081e8bc457a05d82d2
Jeff Fessler <JeffFessler@users.noreply.github.com>
2019-12-12 11:39:29 -0500

    alphabetize

--- src/mri/z-test.jl
+++ src/mri/z-test.jl
@@ -7,5 +7,5 @@ using Test: @test
 @test ir_mri_coil_compress(:test)
 @test ir_mri_kspace_ga_radial(:test)
 @test ir_mri_sensemap_sim(:test)
-@test mri_objects(:test)
 @test mri_kspace_spiral(:test)
+@test mri_objects(:test)
