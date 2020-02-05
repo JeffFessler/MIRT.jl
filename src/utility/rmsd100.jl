8bdb1991d0a296787a17980a178a5004933458c0
Jeff Fessler <JeffFessler@users.noreply.github.com>
2020-02-04 22:20:02 -0500

    Fix tests

--- src/utility/rmsd100.jl
+++ src/utility/rmsd100.jl
@@ -27,16 +27,17 @@ self test
 """
 function rmsd100(x::Symbol)
     x != :test && throw("Invalid symbol; use :test for testing functions.")
-    a = [1 2; 4 5;7 8]
-    b = [3 2; 4 6;8 7]
-    c = [false true;true false;false false] #avg. 0
-    d = [false false;false false;false true] #avg. 1
-    e = [true true;true true;true false] #average here should be 1,0,1,0,4 = sqrt(6/5) = 109...
-    tests = cat(c,d,e,dims = 3)
-    pred = [0 100 100*sqrt(6/5)]
-    for i in 1:size(tests,1)
-        @test rmsd100(a, b, tests[:,:,i]) == pred[i]
+    a = [1 2; 4 5; 7 8]
+    b = [3 2; 4 6; 8 7]
+    c = [false true; true false; false false] #avg. 0
+    d = [false false; false false; false true] #avg. 1
+    e = [true true; true true; true false] #average here should be 1,0,1,0,4 = sqrt(6/5) = 109...
+    tests = cat(c, d, e, dims = 3)
+    pred = [0, 100, 100*sqrt(6/5)]
+    for i in 1:size(tests)
+        @test rmsd100(a, b ; mask=tests[:,:,i]) == pred[i]
     end
+    @test rmsd100([1, 0], [0, 1]) == 100 # no mask test
 
 #=  not important to support or test.  user can pass mask=ig.mask if needed
     a = ones(300,400)
