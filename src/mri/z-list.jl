0c82d7845af83da693957b164c915cae3b3b4485
Jeff Fessler <fessler@umich.edu>
2020-02-09 20:19:02 -0500

    Alphabetize

--- src/mri/z-list.jl
+++ src/mri/z-list.jl
@@ -1,9 +1,9 @@
 # mri/z-list.jl
 
+include("exp_mult.jl")
 include("exp_xform.jl")
 include("coil_compress.jl")
 include("image_geom_mri.jl")
 include("kspace.jl")
 include("mri_objects.jl")
 include("sensemap-sim.jl")
-include("exp_mult.jl")
