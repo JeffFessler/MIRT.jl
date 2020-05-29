e75f9b333d64ac2a671da6a09c92bb35a36f68da
StevenWhitaker <steventwhitaker@gmail.com>
2020-05-29 12:54:12 -0400

    Add `diffnd_adj` and `diffnd_map`.

--- src/regularize/diffs.jl
+++ src/regularize/diffs.jl
@@ -7,7 +7,8 @@ finite differences
 export diff_map
 
 using LinearMapsAA: LinearMapAA
-using Test: @test
+using Test: @test, @test_throws
+using BenchmarkTools: @btime
 
 
 """
@@ -32,8 +33,6 @@ function diff2d_forw(x::AbstractMatrix{<:Number})
 	return [diff(x,dims=1)[:]; diff(x,dims=2)[:]]
 end
 
-# TODO: It could be faster to have a preallocated array for storing all the
-# differences; the array would need to be stored with the LinearMapAA
 function diffnd_forw(x::AbstractArray{<:Number,D}) where {D}
     return reduce(vcat, vec(diff(x, dims = d)) for d = 1:D)
 end
@@ -77,6 +76,59 @@ function diff2d_adj(d::AbstractVector{<:Number}, M::Int, N::Int ; out2d=false)
 	return out2d ? z : z[:] # array or vector?
 end
 
+# Note that N must be strictly greater than 1 for each dimension
+function diffnd_adj(d::AbstractVector{<:Number}, N::Int... ; outnd=false)
+
+    ndims = length(N)
+    length(d) != sum(*(N[1:i-1]..., N[i] - 1, N[i+1:end]...) for i = 1:ndims) &&
+        throw("length(d)")
+
+#    get_partition = i -> begin
+#        if i == 1
+#            di = d[1:*(N[i] - 1, N[i+1:end]...)]
+#        else
+#            start = 1 + sum(*(N[1:i-n-1]..., N[i-n] - 1, N[i-n+1:end]...) for n = 1:i-1)
+#            len = *(N[1:i-1]..., N[i] - 1, N[i+1:end]...)
+#            di = d[start:start+len-1]
+#        end
+#        di = reshape(di, N[1:i-1]..., N[i] - 1, N[i+1:end]...)
+#        zi = cat(-reshape(selectdim(di, i, 1), N[1:i-1]..., 1, N[i+1:end]...),
+#                 (reshape(selectdim(di, i, n) - selectdim(di, i, n + 1), N[1:i-1]..., 1, N[i+1:end]...)
+#                  for n = 1:N[i]-2)...,
+#                 reshape(selectdim(di, i, N[i] - 1), N[1:i-1]..., 1, N[i+1:end]...),
+#                 dims = i)
+#    end
+#    z = sum(get_partition(i) for i = 1:ndims)
+
+    z = Array{eltype(d)}(undef, N...)
+    di = @view(d[1:*(N[1] - 1, N[2:end]...)])
+    di = reshape(di, N[1] - 1, N[2:end]...)
+    slice1 = selectdim(z, 1, 1)
+    slice1 .= -selectdim(di, 1, 1)
+    for n = 2:N[1]-1
+        slicen = selectdim(z, 1, n)
+        slicen .= selectdim(di, 1, n - 1) - selectdim(di, 1, n)
+    end
+    sliceN = selectdim(z, 1, N[1])
+    sliceN .= selectdim(di, 1, N[1] - 1)
+    for i = 2:ndims
+        start = 1 + sum(*(N[1:i-n-1]..., N[i-n] - 1, N[i-n+1:end]...) for n = 1:i-1)
+        len = *(N[1:i-1]..., N[i] - 1, N[i+1:end]...)
+        di = @view(d[start:start+len-1])
+        di = reshape(di, N[1:i-1]..., N[i] - 1, N[i+1:end]...)
+        slice1 = selectdim(z, i, 1)
+        slice1 .-= selectdim(di, i, 1)
+        for n = 2:N[i]-1
+            slicen = selectdim(z, i, n)
+            slicen .+= selectdim(di, i, n - 1) - selectdim(di, i, n)
+        end
+        sliceN = selectdim(z, i, N[i])
+        sliceN .+= selectdim(di, i, N[i] - 1)
+    end
+
+    return outnd ? z : vec(z)
+end
+
 
 """
 `T = diff2d_map(M::Int, N::Int)`
@@ -86,6 +138,14 @@ function diff2d_map(M::Int, N::Int)
         x -> diff2d_forw(reshape(x,M,N)),
         d -> diff2d_adj(d, M, N),
         (N*(M-1)+M*(N-1), N*M), (name="diff2_map",))
+end
+
+function diffnd_map(N::Int...)
+    return LinearMapAA(
+    x -> diffnd_forw(reshape(x,N...)),
+    d -> diffnd_adj(d, N...),
+    (sum(*(N[1:i-1]..., N[i] - 1, N[i+1:end]...) for i = 1:length(N)), prod(N)),
+    (name="diffn_map",))
 end
 
 
@@ -114,4 +174,30 @@ function diff_map(test::Symbol)
 	@test Matrix(T)' == Matrix(T') # adjoint test
 	@test T.name == "diff2_map"
 	true
+end
+
+function diffnd_map(test::Symbol)
+    test != :test && throw(ArgumentError("test $test"))
+    for N in [(2,), (10,), (2,3), (10,11), (2,3,4), (4,4,4,4)]
+        T = diffnd_map(N...)
+        @test Matrix(T)' == Matrix(T')
+        @test T.name == "diffn_map"
+    end
+    # adjoint doesn't work if any of the dimensions has size 1
+    N = (1,2)
+    T = diffnd_map(N...)
+    @test_throws BoundsError Matrix(T)' == Matrix(T')
+    # compare to diff_map
+    N = (100, 101)
+    T2d = diff_map(N...)
+    Tnd = diffnd_map(N...)
+    x = randn(prod(N))
+    d2d = @btime $T2d * $x
+    dnd = @btime $Tnd * $x
+    @test d2d == dnd
+    d = d2d
+    y2d = @btime $T2d' * $d
+    ynd = @btime $Tnd' * $d
+    @test y2d == ynd
+    true
 end
