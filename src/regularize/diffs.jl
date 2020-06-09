07616a1445e45bbbdd12d2e5355ea23c60cac473
Jeff Fessler <fessler@umich.edu>
2020-06-09 15:48:41 -0400

    Tests / dims type / adjoint returns array

--- src/regularize/diffs.jl
+++ src/regularize/diffs.jl
@@ -11,75 +11,117 @@ using LinearMapsAA: LinearMapAA
 using Test: @test, @test_throws
 
 
-"""
-    d = diffnd_forw(X ; dims=1:ndims(X))
+# size after apply `diff` along `dim`
+diff_size(N::Dims, dim::Int) = (N[1:dim-1]..., N[dim] - 1, N[dim+1:end]...)
 
-N-D finite differences along one or more dimensions,
+# corresponding length
+diff_length(N::Dims, dim::Int) = prod(diff_size(N, dim))
+
+# check argument validity
+function diff_check(N::Dims, dims::AbstractVector{Int})
+    !all(1 .<= dims .<= length(N)) && throw(ArgumentError("dims range $dims"))
+    (length(dims) > 1) && (unique(dims) != dims) &&
+        throw(ArgumentError("non-unique dims $dims"))
+    any(N[dims] .== 1) && throw(ArgumentError("invalid size $N for dims $dims"))
+end
+
+
+"""
+    d = diffnd_forw(X ; dims::AbstractVector{Int} = 1:ndims(X))
+
+Finite differences along one or more dimensions of an array,
 e.g., for anisotropic TV regularization.
-Performs the same operations as
+
+By default performs the same operations as
 ``d = [(I_{N_d} \\otimes \\cdots \\otimes D_{N_1}); \\dots; (D_{N_d} \\otimes \\cdots \\otimes I_{N_1})] X[:]``
 where ``D_N`` denotes the `N-1 × N` 1D finite difference matrix
-and ``\\otimes`` denotes the Kronecker product,
-but does it efficiently
+and ``\\otimes`` denotes the Kronecker product, but does it efficiently
 without using `spdiagm` (or any `SparseArrays` function).
 
-Input dimension `N` must exceed 1 for each dimension specified by `dims`,
-or must be 1 for all dimensions specified by `dims.`
+Input dimension `N` must exceed 1 for each dimension specified by `dims`.
 
 in
 - `X` `N_1 × ... × N_d` array (typically an N-D image).
 
 option
-- `dims` dimensions along which to perform finite differences; default `1:d`
+- `dims` dimensions along which to perform finite differences; default `1:ndims(X)`
+must have unique elements and be a subset of 1:ndims(X)
 
 out
-- `d` vector of length `N_d*...*(N_1-1) + ... + (N_d-1)*...*N_1`
+- `d` vector of default length `N_d*...*(N_1-1) + ... + (N_d-1)*...*N_1`
 """
-function diffnd_forw(x::AbstractArray{<:Number,D} ; dims=1:D) where {D}
+function diffnd_forw(x::AbstractArray{<:Number,D}
+    ; dims::AbstractVector{Int} = 1:D,
+) where {D}
+	diff_check(size(x), dims)
     return reduce(vcat, vec(diff(x, dims = d)) for d in dims)
 end
 
 
-"""
-    z =  diffnd_adj(d, N... ; dims=1:length(N), out2d=false)
+#=
+I wanted to give flexibility for Ints and Tuples
+but I could not get this to work...
 
-Adjoint of N-D finite differences along both dimensions.
-Performs the same operations as
-``z = [(I_{N_d} \\otimes \\cdots \\otimes D_{N_1}); \\dots; (D_{N_d} \\otimes \\cdots \\otimes I_{N_1})]' * d``
+"""
+    d = diffnd_forw(X ; dims::AbstractVector{Int} = 1:ndims(X))
+"""
+diffnd_forw(x::AbstractArray{<:Number} ; dims::AbstractVector{Int} = 1:ndims(X)) =
+    diffnd_forw(x, dims)
+
+"""
+    d = diffnd_forw(X ; dims::Dims = (1,))
+"""
+diffnd_forw(x::AbstractArray{<:Number} ; dims::Dims = (1,)) =
+    diffnd_forw(x, collect(dims))
+
+"""
+    d = diffnd_forw(X ; dims::Int = 1)
+"""
+diffnd_forw(x::AbstractArray{<:Number} ; dims::Int = 1) =
+    diffnd_forw(x, [dims])
+=#
+
+
+"""
+    Z = diffnd_adj(dx, N... ; dims=1:length(N))
+
+Adjoint of finite differences of arrays along one or more dimensions.
+By default performs the same operations as
+``vec(Z) = [(I_{N_d} \\otimes \\cdots \\otimes D_{N_1}); \\dots; (D_{N_d} \\otimes \\cdots \\otimes I_{N_1})]' * d``
 where D_N denotes the `N-1 × N` 1D finite difference matrix
 and `\\otimes` denotes the Kronecker product,
 but does it efficiently without using `spdiagm` (or any `SparseArrays` function).
 
 in
-- `d` vector of length `N_d*...*(N_1-1) + ... + (N_d-1)*...*N_1`
+- `dx` vector of typical length `N_d*...*(N_1-1) + ... + (N_d-1)*...*N_1`
 - `N...` desired output size
 
 option
 - `dims` dimensions along which to perform adjoint finite differences
 
 out
-- `z` `prod(N)` vector or `N_1 × ... × N_d` array (typically an N-D image)
+- `Z` `N_1 × ... × N_d` array by default
 
 """
-function diffnd_adj(d::AbstractVector{<:Number}, N::Vararg{Int,D} ; dims=1:D) where {D}
+function diffnd_adj(d::AbstractVector{<:Number}, N::Vararg{Int,D}
+    ; dims::AbstractVector{Int} = 1:D,
+) where {D}
 
 # todo: diff2d_adj etc. 1-liners for backward compat
 # todo: N::Dims instead of Vararg?
 
-    size1 = dim -> (N[1:dim-1]..., N[dim] - 1, N[dim+1:end]...)
-
-    length(d) != sum(prod(size1(dim)) for dim in dims) && throw("length(d)")
+    length(d) != sum(diff_length(N,dim) for dim in dims) && throw("length(d)")
 
     z = zeros(eltype(d), N...)
     for (i, dim) in enumerate(dims)
         if i == 1
-            di = @view(d[1:prod(size1(dim))])
+            di = @view(d[1:diff_length(N,dim)])
         else
-            start = 1 + sum(prod(size1(n)) for n in dims[1:i-1])
-            len = prod(size1(dim))
+            start = 1 + sum(diff_length(N,n) for n in dims[1:i-1])
+            len = diff_length(N,dim)
             di = @view(d[start:start+len-1])
         end
-        di = reshape(di, size1(dim))
+        di = reshape(di, diff_size(N,dim))
         slice1 = selectdim(z, dim, 1)
         slice1 .-= selectdim(di, dim, 1)
         slicen = selectdim(z, dim, 2:N[dim]-1)
@@ -93,18 +135,6 @@ end
 
 
 """
-    T = diffnd_map(N::Int... ; dims=1:length(N))
-"""
-function diffnd_map(N::Vararg{Int,D} ; dims=1:D) where {D}
-    return LinearMapAA(
-        x -> diffnd_forw(reshape(x, N...), dims=dims),
-        d -> diffnd_adj(d, N..., dims=dims),
-        (sum(*(N[1:dim-1]..., N[dim] - 1, N[dim+1:end]...) for dim in dims), prod(N)),
-        (name="diffn_map", dims=dims))
-end
-
-
-"""
     T = diff_map(N::Int... ; dims=1:length(N))
 
 in
@@ -113,8 +143,14 @@ in
 out
 - `T` `LinearMapAA` object for regularizing via `T*x`
 """
-function diff_map(N::Vararg{Int,D} ; dims=1:D) where {D}
-    return diffnd_map(N..., dims=dims)
+function diff_map(N::Vararg{Int,D} ; dims::AbstractVector{Int} = 1:D) where {D}
+	diff_check((N...,), dims)
+    return LinearMapAA(
+        x -> diffnd_forw(reshape(x, N...), dims=dims),
+        d -> diffnd_adj(d, N..., dims=dims)[:],
+        (sum(diff_length(N,dim) for dim in dims), prod(N)),
+        (name="diff_map", dims=dims),
+    )
 end
 
 
@@ -124,36 +160,39 @@ self test
 """
 function diff_map(test::Symbol)
     test != :test && throw(ArgumentError("test $test"))
-    for N in [(2,), (10,), (2,3), (10,11), (1,1,1), (2,3,4), (4,4,4,4)]
+    @test_throws ArgumentError diffnd_forw(ones(3), dims=[2])
+    @test_throws ArgumentError diffnd_forw(ones(3), dims=[1,2])
+    @test_throws ArgumentError diffnd_forw(ones(1,2,1), dims=[1,2])
+    @test diffnd_forw(ones(2,4,6) ; dims=[2]) == zeros(2*3*6)
+
+    for N in ([2,], [10,], [2,3], [10,11], [2,3,4], [4,4,4,4]) # [1,1,1],
         T = diff_map(N...)
         @test Matrix(T)' == Matrix(T')
-        @test T.name == "diffn_map"
-        T = diff_map(N..., dims=1)
+        @test T.name == "diff_map"
+        T = diff_map(N..., dims=[1])
         @test Matrix(T)' == Matrix(T')
-        @test T.name == "diffn_map"
         if length(N) >= 2
-            for dims in [2, (1,2)]
+            for dims in [[2], [1,2]]
                 T = diff_map(N..., dims=dims)
                 @test Matrix(T)' == Matrix(T')
-                @test T.name == "diffn_map"
             end
         end
         if length(N) >= 3
-            for dims in [3, (1,3), (2,3), (1,2,3)]
+            for dims in [[3], [1,3], [2,3], [1,2,3]]
                 T = diff_map(N..., dims=dims)
                 @test Matrix(T)' == Matrix(T')
-                @test T.name == "diffn_map"
             end
         end
     end
+
     # adjoint doesn't work if any of the dimensions specified by dims has size 1
     # (unless all are size 1)
     N = (1,2)
-    T = diff_map(N...)
-    @test_throws BoundsError Matrix(T)' == Matrix(T')
-    T = diff_map(N..., dims = 1)
-    @test Matrix(T)' == Matrix(T')
-    T = diff_map(N..., dims = 2)
+    @test_throws ArgumentError T = diff_map(N...)
+    @test_throws ArgumentError T = diff_map(N..., dims = [1])
+    T = diff_map(N..., dims = [2])
     @test Matrix(T)' == Matrix(T')
     true
 end
+
+# diff_map(:test)
