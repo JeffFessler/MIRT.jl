f1b5258af1c7e4d8205ddf6552a530432b22e2a9
Jeff Fessler <fessler@umich.edu>
2020-06-23 06:49:57 -0400

    Fix timing

--- time/diffl.jl
+++ time/diffl.jl
@@ -3,7 +3,8 @@ diffl.jl
 timing tests
 =#
 
-using MIRT: diffl!, diff_forw
+using MIRT: diffl!, diffl_adj!, diffl, diffl_adj # new
+using MIRT: diff_forw, diff_adj # old
 using Test: @test
 using BenchmarkTools: @btime
 
@@ -13,8 +14,10 @@ using BenchmarkTools: @btime
 =#
 
 
-# time vs diff
-function time0()
+function diffl_time()
+
+	@info "time vs diff and vs w/o in-place"
+
 	N = (2^10, 2^9)
     x = rand(N...)
     g0 = rand(N...)
@@ -22,35 +25,14 @@ function time0()
 	g0 = diff(x, dims=2)
     diffl!(g1, x, 2)
 	@test g1[:,2:end] == g0
-    @btime diffl!($g1, $x, 2)		# 187 528 b
-    @btime g0 = diff($x, dims=2)	# 305 3.99 MiB
-    @btime g1 = diffl($x, 2)		# 305 4.00 MiB
-	nothing
-end
+    @btime diffl!($g1, $x, 2)		# 210 20 672 b
+    @btime g0 = diff($x, dims=2)	# 303  4 3.99 MiB
+    @btime g1 = diffl($x, 2)		# 306 22 4.00 MiB
 
-function time1()
-    N = 2^10
-    x = rand(N)
-    g = rand(N)
-    @btime diffl!($g, $x)
-    @btime diffl!($g, $x ; add=true)
-    @btime g = diffl($x)
-    nothing
-end
 
-function time2()
-	N = (2^10, 2^9)
-    x = rand(N...)
-    g = rand(N...)
-    @btime diffl!($g, $x, 1)
-    @btime g = diffl($x)
-    nothing
-end
+	@info "time vs diff_forw"
+	# in-place version diffl! about 40% faster than diff_forw
 
-using MIRT: diff_forw, diff_adj
-
-# in-place version diffl! about 40% faster than diff_forw
-function time3()
 	N = (2^5, 2^6, 2^7)
     x = rand(N...)
 	d = 2
@@ -59,8 +41,22 @@ function time3()
 	@test vec(g2[:,2:end,:]) == h2
     diffl!(g2, x, d)
 	@test vec(g2[:,2:end,:]) == h2
-    @btime diffl!($g2, $x, $d)
-    @btime diff_forw($x, dims=$d)
+    @btime diffl!($g2, $x, $d)		# 92 24 800 b
+    @btime diff_forw($x, dims=$d)	# 148 6 1.97 MiB
+
+
+	@info "time adj" # about 40% speed up
+
+	z2 = diff_adj(h2, N ; dims=d)
+	z1 = diffl_adj(g2, d)
+	@test z1 == z2
+	diffl_adj!(z1, g2, d)
+	@test z1 == z2
+
+	@btime diffl_adj!(z1, g2, d)		# 361 28 928 b
+	@btime diff_adj($h2, $N ; dims=$d)	# 607 70 3.94 MiB
+
+	nothing
 end
 
-#time0()
+#diffl_time()
