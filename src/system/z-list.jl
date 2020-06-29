0913e92c357e5e5a87bbdde889b8edd30aafa05c
Jeff Fessler <fessler@umich.edu>
2020-06-29 15:50:18 -0400

    Purge LinearMap tests

--- src/system/z-list.jl
+++ src/system/z-list.jl
@@ -1,6 +1,3 @@
 # system/z-list,jl
 
 include("Afft.jl")
-include("linear-map/block.jl")
-include("linear-map/getindex.jl")
-include("linear-map/hcat.jl")
--- src/system/z-test.jl
+++ src/system/z-test.jl
@@ -2,7 +2,4 @@
 
 using Test: @test
 
-@test block_lm(:test)
-@test ir_lm_test_getindex_cumsum()
-@test hcat_lm(:test)
 @test Afft(:test)
