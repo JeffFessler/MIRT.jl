3a4b1e33a35e2ab062f532a22866bfb11f6e5cd5
Jeff Fessler <JeffFessler@users.noreply.github.com>
2020-11-24 22:17:21 -0500

    add comment

--- examples/grayscale-shepp-logan.jl
+++ examples/grayscale-shepp-logan.jl
@@ -1,4 +1,4 @@
+# illustrate how to make and display the Shepp-Logan ellipse digital image phantom
 using MIRT: jim, ellipse_im
 x = ellipse_im(100)
-jim(x, title="test")
-
+jim(x, title="Shepp-Logan Phantom")
