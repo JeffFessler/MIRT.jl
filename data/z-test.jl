dddff1c21bf94a7d0d039faf5b2d6a22cc3a09ed
Jeff Fessler <fessler@umich.edu>
2021-05-21 13:04:04 -0400

    brain test

--- data/z-test.jl
+++ data/z-test.jl
@@ -1,5 +1,9 @@
 # data/z-test.jl
 
-using Test
+using MIRT: ir_load_brainweb_t1_256
+using Test: @test, @testset
 
-@test extrema(ir_load_brainweb_t1_256()) == (0.0f0, 241.0f0)
+@testset brainweb begin
+    x = ir_load_brainweb_t1_256()
+    @test extrema(x) == (0.0f0, 241.0f0)
+end
