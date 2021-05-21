ad34d4a15de7e6ec35e4c01f3230db0440e62c99
Jeff Fessler <fessler@umich.edu>
2021-05-21 15:54:37 -0400

    deprecate

--- src/io/fld-read.jl
+++ src/io/fld-read.jl
@@ -5,7 +5,16 @@ Jeff Fessler
 
 export fld_header, fld_read
 
+using AVSfldIO # temporary?
+import FileIO # temporary
 
+
+@deprecate fld_header AVSfldIO.fld_header
+#@deprecate fld_read AVSfldIO.fld_read
+@deprecate fld_read FileIO.load
+
+
+#=
 """
     fld_header()
 
@@ -24,3 +33,4 @@ Deprecated: use `FileIO.load` instead (see `AVSfldIO.fld_read`).
 function fld_read()
     throw("fld_read is deprecated: use FileIO.load instead.")
 end
+=#
--- src/io/fld-write.jl
+++ src/io/fld-write.jl
@@ -5,7 +5,14 @@ Jeff Fessler, University of Michigan
 
 export fld_write
 
+#using AVSfldIO # temporary
+import FileIO # temporary
 
+#@deprecate fld_write AVSfldIO.fld_write
+@deprecate fld_write FileIO.save
+
+
+#=
 """
     fld_write()
 
@@ -14,3 +21,4 @@ Deprecated: use `FileIO.save` instead (see `AVSfldIO.fld_write`).
 function fld_write()
     throw("fld_write is deprecated: use `FileIO.save` instead")
 end
+=#
