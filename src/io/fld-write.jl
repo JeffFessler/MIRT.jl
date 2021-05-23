7a552a863ea56fd07e8176fb8c01bb6c832cf281
Jeff Fessler <JeffFessler@users.noreply.github.com>
2021-05-23 10:59:33 -0400

    deprecate jim : moved to MIRTjim.jim (#96)
    * jim deprecate
    
    * spell
    
    * all signatures
    
    * kwargs
    
    * goodbye Plots
    
    * work around pkg server delay (#97)
    
    * test/Project
    
    * compat
    
    * to 0.14.0
    
    Co-authored-by: Johnny Chen <johnnychen94@hotmail.com>

--- src/io/fld-write.jl
+++ src/io/fld-write.jl
@@ -5,20 +5,6 @@ Jeff Fessler, University of Michigan
 
 export fld_write
 
-#using AVSfldIO # temporary
 import FileIO # temporary
 
-#@deprecate fld_write AVSfldIO.fld_write
-@deprecate fld_write FileIO.save
-
-
-#=
-"""
-    fld_write()
-
-Deprecated: use `FileIO.save` instead (see `AVSfldIO.fld_write`).
-"""
-function fld_write()
-    throw("fld_write is deprecated: use `FileIO.save` instead")
-end
-=#
+@deprecate fld_write(args... ; kwargs...) FileIO.save(args... ; kwargs...)
