ce3806426542a4afb006ec5ba415f8a7fabc38a9
Jeff Fessler <fessler@umich.edu>
2021-05-23 07:23:58 -0400

    all signatures

--- src/plot/jim.jl
+++ src/plot/jim.jl
@@ -8,4 +8,7 @@ export jim
 
 import MIRTjim # jim
 
-@deprecate jim MIRTjim.jim
+@deprecate jim(a ; kwargs...) MIRTjim.jim(a ; kwargs...)
+@deprecate jim(a, b ; kwargs...) MIRTjim.jim(a, b ; kwargs...)
+@deprecate jim(a, b, c ; kwargs...) MIRTjim.jim(a, b, c ; kwargs...)
+@deprecate jim(a, b, c, d ; kwargs...) MIRTjim.jim(a, b, c, d ; kwargs...)
