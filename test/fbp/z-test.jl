1faf9a1fabe99e5628a8be686f174ba8ce421797
Jeff Fessler <fessler@umich.edu>
2022-09-09 13:27:08 -0400

    code cov

--- test/fbp/z-test.jl
+++ test/fbp/z-test.jl
@@ -1,6 +1,7 @@
 # fbp/z-test.jl
 
-using Test: @testset
+using Test: @testset, @test_throws
+import MIRT
 
 list = [
 "rotate2d.jl"
@@ -10,4 +11,19 @@ for file in list
 	@testset "$file" begin
 		include(file)
 	end
+end
+
+@testset "deprecate" begin
+ @test_throws String MIRT.image_geom()
+ @test_throws String MIRT.image_geom_mri()
+ @test_throws String MIRT.cuboid_im()
+ @test_throws String MIRT.disk_phantom_params()
+ @test_throws String MIRT.ellipse_im()
+ @test_throws String MIRT.ellipse_im_params()
+ @test_throws String MIRT.ellipse_sino()
+ @test_throws String MIRT.ellipsoid_im()
+ @test_throws String MIRT.rect_im()
+ @test_throws String MIRT.rect_sino()
+ @test_throws String MIRT.sino_geom()
+ @test_throws String MIRT.sino_plot()
 end
--- test/io/ir_dump.jl
+++ test/io/ir_dump.jl
@@ -6,4 +6,7 @@ using Test: @test
 
 
 x = (int=1, real=2., string="String", shorttuple=(1,2), longtuple=(1,2,3,4), )
-@test ir_dump(x, io=isinteractive() ? stdout : IOBuffer()) isa Nothing
+io = isinteractive() ? stdout : IOBuffer()
+@test ir_dump(x; io) isa Nothing
+x = trues(1)
+@test ir_dump(x; io) isa Nothing
