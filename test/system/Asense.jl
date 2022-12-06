aa7d59315d7dda1a40f1619c08e34f6c7074bf57
Jeff Fessler <fessler@umich.edu>
2022-12-05 23:50:12 -0500

    Test plan

--- test/system/Asense.jl
+++ test/system/Asense.jl
@@ -27,3 +27,14 @@ using Test: @test, @testset
     y = randn(T, count(samp), ncoil)
     @test isapprox(dot(y, A * x), dot(A' * y, x))
 end
+
+@testset "Asense-opt" begin
+    sdim = (3,4,2)
+    samp = trues(sdim); samp[2] = false
+    T = ComplexF32
+    smaps = [randn(T, sdim), randn(T, sdim)]
+    dims = (1, 3)
+	A = Asense(samp, smaps; dims, fft_forward=false)
+	@test Matrix(A)' ≈ Matrix(A')
+    x = randn(T, sdim)
+end
