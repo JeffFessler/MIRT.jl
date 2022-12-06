59c575b53114a4178c39ce352011c05cea3b07d7
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-12-06 15:32:06 -0500

    Test unitary Asense

--- test/system/Asense.jl
+++ test/system/Asense.jl
@@ -1,8 +1,10 @@
 # test/Asense.jl
 
-using MIRT: Asense
+using MIRT: Asense, embed
 using LinearMapsAA: LinearMapAO
+using FFTW: fft, bfft, fftshift, ifftshift
 using LinearAlgebra: dot
+
 using Test: @test, @testset
 
 @testset "Asense" begin
@@ -29,12 +31,25 @@ using Test: @test, @testset
 end
 
 @testset "Asense-opt" begin
+end
     sdim = (3,4,2)
     samp = trues(sdim); samp[2] = false
     T = ComplexF32
     smaps = [randn(T, sdim), randn(T, sdim)]
-    dims = (1, 3)
-	A = Asense(samp, smaps; dims, fft_forward=false)
-	@test Matrix(A)' ≈ Matrix(A')
-    x = randn(T, sdim)
-end
+    dims = [1, 3]
+    sqrtN = sqrt(prod(sdim[dims]))
+    for unitary in (false, true)
+	    A = Asense(samp, smaps; dims, fft_forward=true, unitary)
+        factor = unitary ? 1/sqrtN : 1
+	    @test Matrix(A)' ≈ Matrix(A')
+
+        forw(x, smap) = fftshift(fft(ifftshift(x .* smap), dims))[samp] * T(factor)
+        x = randn(T, sdim)
+        y = [forw(x, smaps[1]) forw(x, smaps[2])]
+        @test A * x ≈ y
+
+        back(z, smap) = fftshift(bfft(ifftshift(z), dims)) .* conj(smap) * T(factor)
+        b = back(embed(y[:,1], samp), smaps[1]) +
+            back(embed(y[:,2], samp), smaps[2])
+        @test A' * y ≈ b
+    end
