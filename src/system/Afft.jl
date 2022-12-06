2cdb2484bd9cf37024b4dad13ccda6ee5e1de6db
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-12-06 13:44:15 -0500

    Refine Afft

--- src/system/Afft.jl
+++ src/system/Afft.jl
@@ -14,17 +14,21 @@ using FFTW: plan_fft!, plan_bfft!
 
 
 """
-    A = Afft(xdim::Dims, fdim ; T, operator, work, ...)
-Make a `LinearMapAO` operator object
-for FFT of size `xdim` along dimensions `fdim`, of type `T`.
+    A = Afft(xdim::Dims, fdim ; T, operator, unitary, work, ...)
+Make a `<:LinearMapAX` operator object
+for the FFT of an array of size `xdim`,
+along dimensions `fdim`,
+of type `T`.
 
 In:
-- `xdim::Dims{D}` dimensions of signal
+- `xdim::Dims{D}` array size
 - `fdim = 1:D` apply fft/bfft only along these dimensions
 
 Option:
 - `T::DataType = ComplexF32`
-- `operator::Bool = true` set to `false` to return a `LinearMapAM`
+- `operator::Bool = true` return a `LinearMapAO`
+  set to `false` to return a `LinearMapAM`
+- `unitary::Bool = false` set to `true` for unitary DFT
 - `work::AbstractArray` work space for in-place fft operations
 - remaining arguments are passed to `plan_fft`
 
@@ -38,54 +42,61 @@ function Afft(
     T::DataType = ComplexF32,
     operator::Bool = true, # !
     unitary::Bool = false,
-    work::AbstractArray{S,D} = Array{T,D}(undef, xdim),
+    work::AbstractArray{Tw,D} = Array{T,D}(undef, xdim),
     kwargs...
-) where {D}
+) where {D, Tw <: Number}
 
-    promote_type(S, T) == S || error("type S=$S cannot hold T=$T")
-    size(work) == xdim || error("mismatch: work xdim")
+    all(in(1:D), fdim) || error("fdim $fdim")
+    promote_type(Tw, T) == Tw || error("type Tw=$Tw cannot hold T=$T")
+    size(work) == xdim || error("work size mismatch")
 
+    # "work" is essential below because the plans are designed
+    # for eltype T only and x,y might have different eltype.
     pf = plan_fft!(work, fdim; kwargs...)
     pb = plan_bfft!(work, fdim; kwargs...)
-    if !fft_forward
-        (pf, pb) = (pb, pf)
-    end
 
-    sqrtN = sqrt(prod(xdim))
-    function forw!(y, x::AbstractArray{<:Real})
-        @. work = complex(x)
-        mul!(y, pf, work)
-        unitary && y ./= sqrtN
+    N = prod(xdim[fdim])
+
+    function fun!(y::AbstractArray{Ty}, x, plan, factor::Real,
+    ) where {Ty <: Number}
+        if Ty == Tw
+            if factor != 1
+                @. y = x * factor
+            else
+                copyto!(y, x)
+            end
+            mul!(y, plan, y) # no "work" needed in this case
+        else
+            if factor != 1
+                @. work = x * factor
+            else
+                copyto!(work, x)
+            end
+            mul!(work, plan, work)
+            copyto!(y, work)
+        end
         return y
     end
-    function forw!(y, x)
-        copyto!(y, x)
-        mul!(y, pf, y)
-        unitary && y ./= sqrtN
-        return y
-    end
 
-    function back!(x, y)
-        copyto!(x, y)
-        unitary && x .*= sqrtN # todo merge copyto!
-        mul!(x, pb, x)
-        return x
-    end
+    Tr = real(T)
+    factor = unitary ? Tr(1/sqrt(N)) : 1
+    forw!(y, x) = fun!(y, x, pf, factor)
+    back!(x, y) = fun!(x, y, pb, factor)
 
-    prop = (; name="fft", fdim)
+    prop = (; name="fft", fdim, unitary, pf, pb)
 
     if operator
         return LinearMapAA(forw!, back!,
-            (1, 1) .* prod(dim), prop ; T,
+            (1, 1) .* prod(xdim), prop ; T,
             idim = xdim, odim = xdim,
             operator,
         )
     end
 
     return LinearMapAA(
-        (y,x) -> forw!(y, reshape(x,xdim)), # fft(reshape(x,dim))
-        (x,y) -> vec(back!(reshape(x,xdim),y)), # vec(bfft(y))
-        (sum(samp), prod(dim)), prop ; T,
+        (y,x) -> vec(forw!(reshape(y,xdim), reshape(x,xdim))),
+        (x,y) -> vec(back!(reshape(x,xdim), reshape(y,xdim))),
+        (1, 1) .* prod(xdim), prop ; T,
     )
 end
 
@@ -114,12 +125,12 @@ function Afft(
     dims = 1:D,
     T::DataType = ComplexF32,
     operator::Bool = true, # !
-    work::AbstractArray{S,D} = similar(samp, T),
+    work::AbstractArray{Tw,D} = similar(samp, T),
     fft_forward::Bool = true,
     kwargs...
-) where {D, S <: Number}
+) where {D, Tw <: Number}
 
-    promote_type(S, T) == S || error("type S=$S cannot hold T=$T")
+    promote_type(Tw, T) == Tw || error("type Tw=$Tw cannot hold T=$T")
     axes(work) == axes(samp) || error("axes mismatch: samp work")
 
     pf = plan_fft!(work, dims; kwargs...)
@@ -129,12 +140,6 @@ function Afft(
     end
 
 #   forw! = (y,x) -> getindex!(y, fft!(copyto!(work,x)), samp)
-    function forw!(y, x::AbstractArray{<:Real})
-        @. work = complex(x)
-        mul!(work, pf, work)
-        getindex!(y, work, samp)
-        return y
-    end
     function forw!(y, x)
         copyto!(work, x)
         mul!(work, pf, work)
@@ -143,9 +148,15 @@ function Afft(
     end
 
 #   back! = (x,y) -> bfft!(embed!(x,y,samp))
-    function back!(x, y)
-        embed!(x, y, samp)
-        mul!(x, pb, x)
+    function back!(x::AbstractArray{<:Tx}, y) where {Tx <: Number}
+        if Tx == Tw
+            embed!(x, y, samp)
+            mul!(x, pb, x) # no work needed
+        else
+            embed!(work, y, samp)
+            mul!(work, pb, work)
+            copyto!(x, work)
+        end
         return x
     end
 
